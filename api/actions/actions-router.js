3ea108e3793caa68128d5ce781a2e56873d6df76
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-04 10:10:36 -0700

    Completed, server,brought in routers

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,11 @@
 // Write your "actions" router here!
+const express = require("express")
+const Actions = require('./actions-model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.json({message:"Hello form Actions ROUTER!!!!"})
+})
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,12 @@
 // Write your "projects" router here!
+const express = require("express")
+const Projects = require('./projects-model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.json({message:"Hello from insite the ROUTER"})
+})
+
+
+module.exports = router
