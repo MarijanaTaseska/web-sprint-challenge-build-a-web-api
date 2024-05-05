c53cfc114f1b158c569f7c80557b0e70461a260c
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-05 14:52:21 -0700

    Delete Project

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -34,10 +34,51 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
 try{
-
+const {name, description} = req.body;
+if(!name || !description){
+    res.status(400).json({message:" name and description are required"})
+}else{
+    const createProject = await Projects.insert(req.body)
+    res.status(201).json(createProject)
+ }
 }
 catch(err){
-    res.status(500).json({message: "Error posting project"})
+    res.status(500).json({message: `Error posting project ${err.message}`})
+}
+})
+
+router.put('/:id',async (req, res) => {
+try{
+const { id } = req.params
+const {name, description, completed} = req.body
+if(!name || !description || completed === undefined){
+    res.status(400).json({message: "Must provide name, completed and description to update"})
+}else{
+    const project = await Projects.get(id)
+    if(!project){
+        res.status(404).json({message:"no projects with this id"})
+    }else{
+     const updateProject = await Projects.update(id,{name, description, completed})
+     res.status(200).json(updateProject)
+    }
+ }
+ }
+catch(err){
+    res.status(500).json({message: `Error updating project ${err.message}`}) 
+}
+})
+
+router.delete('/:id',async (req, res) => {
+try{
+    const projectID = await Projects.get(req.params.id)
+    if(!projectID){
+        res.status(404).json({message:"no project with this id"})
+    }else{
+      const deletedProject = await Projects.remove(projectID.id)
+      res.json({message:`you successfully deleted ${deletedProject} project`})
+    }
+}catch(err){
+    res.status(500).json({message: `Error updating project ${err.message}`}) 
 }
 })
 
