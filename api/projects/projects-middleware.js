b00d0cd74e4a2c8bbccc648302f1aec30b178452
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-07 10:09:31 -0700

    Sprint 13 Challenge submission

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -2,28 +2,29 @@
 const Projects = require("./projects-model")
 
 
-async function validateProjectId(req, res, next){
-    try{   
+async function validateProjectId(req, res, next) {
+    try {
         const project = await Projects.get(req.params.id)
-        if(!project){
-            res.status(404).json({message: "No project with a given ID"})
-        }else{
+        if (!project) {
+            res.status(404).json({ message: "No project with a given ID" })
+        } else {
             req.project = project
             next()
         }
-    } catch(err){
-        res.status(500).json({message:"Error retrieving project"})
+    } catch (err) {
+        res.status(500).json({ message: "Error retrieving project" })
     }
 }
 
- function validateProjectBody(req, res, next){ const { name, description }  = req.body
-        if(!name || !description){
-            res.status(400).json({message: "Must provide name and description"})
-        }else{
-            req.name = name;
-            req.description = description;
-            next()
-         }
+function validateProjectBody(req, res, next) {
+    const { name, description } = req.body
+    if (!name || !description) {
+        res.status(400).json({ message: "Must provide name and description" })
+    } else {
+        req.name = name;
+        req.description = description;
+        next()
+    }
 }
 
 module.exports = {
